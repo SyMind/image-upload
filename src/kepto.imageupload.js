806c9b3a43a87ac568d954b02f15d583d7885358
SyMind <dacongsama@live.com>
2017-10-30 07:04:41 +0800

    fix some bug for new image that added by user is view

--- src/kepto.imageupload.js
+++ src/kepto.imageupload.js
@@ -16,19 +16,26 @@
             this.canvas = $('<canvas style="display:none">')[0];
             this.context = this.canvas.getContext('2d');
             this.$button.append(this.$label).append(this.$input);
-            this.$container = $('<div class="image-upload">').append(this.$button),
+            this.$list = $('<div class="image-upload-list">');
+            this.$container = $('<div class="image-upload">').append(this.$list).append(this.$button),
 
             this.$root.append(this.$container);
 
             this.$input.change(() => {
                 let file = this.$input[0].files[0],
                     reader = new FileReader();
-
+                console.log(file);
                 reader.readAsDataURL(file);
                 reader.onload = (e) => {
-                    this.compress(e.target.result, (compress_data_url) => 
-                        this.$container.prepend($('<div class="image-upload-item"></div>')
-                            .css('background-image', `url(${compress_data_url})`)));
+                    this.compress(e.target.result, (compress_data_url) =>  {
+                        this.$list.append($('<div class="image-upload-item"><div class="image-upload-close"></div></div>')
+                            .css('background-image', `url(${compress_data_url})`));
+                        $.ajax({
+                          url: 'upload',
+                          type: 'POST',
+                          data: new FormData()
+                        })
+                    });
                 }
             });
         }
@@ -37,13 +44,29 @@
             let image = new Image();
             image.src = data_url;
             image.onload = () => {
-                this.context.drawImage(image, 0, 0);
+                this.canvas.height = image.naturalHeight;
+                this.canvas.width = image.naturalWidth;
+                this.context.clearRect(0, 0 , this.canvas.width, this.canvas.height);
+                this.context.drawImage(image, 0, 0, image.naturalWidth, image.naturalHeight);
                 callback(this.canvas.toDataURL('image/jpeg', 0.8));
             }
         }
 
+        dataUrlToBlob(data_url) {
+          let arr = data_url.split(','), mime = arr[0].match()
+        }
+
+        event() {
+          this.$root.click(function(e) {
+            if(e.target.className === 'image-upload-close') {
+              $(e.target.parentNode).remove();
+            }
+          });
+        }
+
         create() {
             this.init();
+            this.event();
         }
     }
     $.extend($.fn, {
